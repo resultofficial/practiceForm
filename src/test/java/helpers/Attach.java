630b8879edc1edbf2c1a37ddea1bd68c53e8826a
Anastasiya_rever <result_official@mail.ru>
2025-02-11 17:26:42 +0700

    added video attach

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -36,14 +36,14 @@ public class Attach {
                 String.join("\n", Selenide.getWebDriverLogs(BROWSER))
         );
     }
-
+//добавляем возможность проигрывать видео на страницу
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
                 + getVideoUrl()
                 + "' type='video/mp4'></video></body></html>";
     }
-
+//достаем ссылку на видео
     public static URL getVideoUrl() {
         String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
         try {
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,9 @@ import com.codeborne.selenide.Configuration;
 import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
     @BeforeAll
@@ -13,12 +16,20 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "eager";//игнорируем медленную загруз.стр
         Configuration.timeout = 5000; // default 4000
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //указываем что бы видео записывалось
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true //вкл выдео
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
     @AfterEach
     void addAttachment(){
         Attach.screenshotAs("Last screenshot");
         Attach.browserConsoleLogs();
         Attach.pageSource();
+        Attach.addVideo();
     }
 
 }
